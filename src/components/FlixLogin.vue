0b3ae754695349656886c30d05684540b2556efc
Bruno Bandeira <brunodev29ab@gmail.com>
2022-01-04 23:25:26 -0300

    WIP 10%

--- src/components/FlixLogin.vue
+++ src/components/FlixLogin.vue
@@ -14,15 +14,52 @@
                     <input class="box" type="text" placeholder="Senha">
                 </form>
                 <button class="btn"> Entrar </button>
+
                 <p class="sub-title"><input type="checkbox"> Lembre-se de mim </p>
-                <p class="sub-title2">Precisa de Ajuda</p>
-                <p class="sub-title3"><i class="fa fa-facebook-square"></i> Conectar com Facebook</p>
-                <p class="sub-title"> Novo por aqui ?<span>Assine agora.</span></p>
-                <p class="sub-title">Essa página é protegida pelo google reCAPTCHA <br>
+
+                <p class="sub-title2"><a href="#"> Precisa de Ajuda ? </a></p>
+
+                <p class="sub-title3"><i class="fa fa-facebook-square"></i><a href="#"> Conectar com Facebook </a></p>
+
+                <p class="sub-title4"> Novo por aqui ?<span> <a href="#"> Assine agora. </a></span></p>
+                
+                <p class="sub-title5">Essa página é protegida pelo google reCAPTCHA <br>
                 para garantir que vc não é um robo <a href="#"> saiba mais</a>
                 </p>
             </div>
         </nav>
+
+        <footer>
+            <div class="footer-painel">
+                    <div class="footer-links-c1">
+                        <a href="#">Dúvidas? Ligue 0800-761-4631</a>
+                        <br>
+                        <a href="#">Perguntas frequentes</a>
+                        <br>
+                        <a href="#">Preferências de cookies</a>
+
+                        <select action="">
+                            <option selected> Português </option>
+                            <option > Inglês </option>
+                        </select>
+                    </div>
+
+                    <div class="footer-links-c2">
+                        <a href="#">Centro de ajuda</a>
+                        <br>
+                        <a href="#">Informações corporativas</a>
+                    </div>
+
+                    <div class="footer-links-c3">
+                        <a href="#">Termos de uso</a>
+                    </div>
+
+                    <div class="footer-links-c4">
+                        <a href="#">Privacidade</a>
+                    </div>
+
+            </div>
+        </footer>
         
     </div>
 </template>
@@ -50,8 +87,9 @@ export default {
 
     .telaDeFundo {
         width: 100vw;
-        height: 120vh;
+        height: 160vh;
         opacity: 0.5;
+        position: relative;
     }
 
     .login-box {
@@ -108,22 +146,59 @@ export default {
         color: #fff;
         font-size: 1.8rem;
         font-weight: 700;
-        
     }
 
     .login-box .sub-title {
-        color: #fff;
+        color: #8c8c8c;
+        position: absolute;
     }
 
     .login-box .sub-title2 {
-        color: #fff;
+        color: #8c8c8c;
         display: flex;
         justify-content: flex-end;
         align-items: center;
+        padding-top: 1rem;
+    }
+
+    .login-box .sub-title2 a{
+        color: #8c8c8c;
     }
 
     .login-box .sub-title3 {
+        color: #8c8c8c;
+        margin-bottom: -5px;
+    }
+
+    .login-box .sub-title3 a{
+        color: #8c8c8c;
+    }
+
+
+    .login-box .sub-title4 {
+        color: #8c8c8c;
+        padding-top: 1.5rem;
+    }
+
+    .login-box .sub-title4 span{
         color: #fff;
+        font-size: 1.2rem;
+        font-family: bolder;
+    }
+
+    .login-box .sub-title4 span a{
+        color: #fff;
+
+    }
+
+    .login-box .sub-title5 {
+        color: #8c8c8c;
+        padding: 0.9rem 0;
+        font-size: 0.8rem;
+    }
+
+    .login-box .sub-title5 a{
+        color: #0071eb;
     }
 
 
@@ -141,4 +216,56 @@ export default {
         font-size: 1.8rem ;
     }
 
+    footer {
+        width: 100%;
+        height: 19.8rem;
+        background-color: rgba(0,0,0,.75);
+        position: absolute;
+        bottom: -31rem;
+    }
+
+    footer div a{
+        color: #8c8c8c;
+    }
+
+
+    .footer-painel{
+        padding: 2.5rem;
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .footer-links-c1 {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footer-links-c1 a{
+        padding: 0.8rem 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footer-links-c2 {
+        width: 300px;
+    }
+
+    .footer-links-c2 a{
+        padding: 0.2rem 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footer-links-c3 {
+        width: 300px;
+    }
+
+    .footer-links-c3 a{
+        padding: 0.8rem 0;
+        display: flex;
+        flex-direction: row;
+    }
+
+    
 </style>
