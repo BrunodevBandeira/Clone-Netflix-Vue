605f3ee8b34290407135dad0dbf871c34e90c171
Bruno Bandeira <brunodev29ab@gmail.com>
2022-01-19 20:30:40 -0300

    WIP 19.9% style

--- src/components/FlixLoginBusca.vue
+++ src/components/FlixLoginBusca.vue
@@ -59,6 +59,7 @@
 
         <section class="containeres">
                 <h1 class="container-title"> Perguntas Frequentes </h1>
+            <div>
                 <div class="mini-container"> 
                     <p>O que é a Netflix ?</p><i class="fa fa-plus fa-2x"></i>
                 </div>
@@ -67,8 +68,66 @@
                     <br><br>
                     Você pode assistir a quantos filmes e séries quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes.</p>
                 </div>
+            </div>
                 <!-- <i class="fa fa-times" aria-hidden="true"></i> -->
+            <div>
+                <div class="mini-container"> 
+                    <p>Quanto custa a Netflix ?</p><i class="fa fa-plus fa-2x"></i>
+                </div>
+                <div class="sub-container">
+                    <p>Assista à Netflix no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$25,90 a R$55,90 por mês. Sem contrato nem taxas extras.</p>
+                </div>
+            </div>
+
+            <div>
+                <div class="mini-container"> 
+                    <p>Onde posso assistir ?</p><i class="fa fa-plus fa-2x"></i>
+                </div>
+                <div class="sub-container">
+                    <p>Assista onde quiser, quando quiser. Faça login com sua conta Netflix em netflix.com para começar a assistir no computador ou em qualquer aparelho conectado à Internet compatível com o aplicativo Netflix, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
+                    <br><br>
+                    Você também pode baixar a sua série favorita com o aplicativo Netflix para iOS, Android ou Windows 10. Use downloads para levar a Netflix para onde quiser sem precisar de conexão com a Internet. Leve a Netflix com você para qualquer lugar.</p>
+                </div>
+            </div>
+
+            <div>
+            <div class="mini-container"> 
+                    <p>Como faço para cancelar ?</p><i class="fa fa-plus fa-2x"></i>
+                </div>
+                <div class="sub-container">
+                    <p>A Netflix é flexível. Não há contratos nem compromissos. Você pode cancelar a sua conta na internet com apenas dois cliques. Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.</p>
+                </div>
+            </div>
+
+            <div>
+                <div class="mini-container"> 
+                    <p>Oque eu posso assistir na Netflix ?</p><i class="fa fa-plus fa-2x"></i>
+                </div>
+                <div class="sub-container">
+                    <p>A Netflix tem um grande catálogo de filmes, documentários, séries, originais Netflix premiados e muito mais. Assista o quanto quiser, quando quiser.</p>
+                </div>
+            </div>
+
+            <div>
+                <div class="mini-container"> 
+                    <p>A Netflix é adequada para crianças ?</p><i class="fa fa-plus fa-2x"></i>
+                </div>
+                <div class="sub-container">
+                    <p>A experiência infantil da Netflix faz parte da sua assinatura para que as crianças se divirtam em seu próprio espaço com séries e filmes familiares sob a supervisão dos pais.
+                    <br><br>
+                    O recurso de controle dos pais, incluso nos perfis para crianças e protegido por PIN, permite restringir a classificação etária do conteúdo que as crianças podem ver e bloquear títulos específicos que você não quer que elas assistam.</p>
+                </div>
+            </div>
+
+        <nav class="">
+            <p class="p-title"> Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+            <form class="form" method="GET">
+                <input type="text" placeholder="Buscar filme" name="movie" class="form__input" >
+                <input type="submit" class="form__submit" value="Vamos lá">
+            </form>
+        </nav>
         </section>
+        
 
         <footer>
             saspm
@@ -302,6 +361,7 @@
         display: flex;
         justify-content: center;
         justify-content: space-between;
+        margin: 0.5rem 0;
     }
 
     .mini-container p {
